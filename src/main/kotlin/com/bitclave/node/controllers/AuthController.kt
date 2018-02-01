fa4f5721f92e3e8e4183914ab8e0f5bc9eaa7e1c
SprinterGameDev <sprinter@ozersk.com>
2018-02-01 17:08:41 +0500

    add correct response code for signup.

--- src/main/kotlin/com/bitclave/node/controllers/AuthController.kt
+++ src/main/kotlin/com/bitclave/node/controllers/AuthController.kt
@@ -2,10 +2,8 @@ package com.bitclave.node.controllers
 
 import com.bitclave.node.repository.models.Account
 import com.bitclave.node.services.AccountService
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.CompletableFuture
 
 @RestController()
@@ -13,6 +11,7 @@ import java.util.concurrent.CompletableFuture
 class AuthController(private val accountService: AccountService) {
 
     @RequestMapping(method = [RequestMethod.POST], value = ["signUp"])
+    @ResponseStatus(value = HttpStatus.CREATED)
     fun signUp(@RequestBody account: Account): CompletableFuture<Account> {
         return accountService.registrationClient(account)
     }
