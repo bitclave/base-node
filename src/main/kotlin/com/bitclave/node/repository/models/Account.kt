cf7650791c52ebc436ff4eafd49399f474812b13
SprinterGameDev <sprinter@ozersk.com>
2018-02-02 16:05:09 +0500

    ignore method in Account model.

--- src/main/kotlin/com/bitclave/node/repository/models/Account.kt
+++ src/main/kotlin/com/bitclave/node/repository/models/Account.kt
@@ -1,5 +1,6 @@
 package com.bitclave.node.repository.models
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -11,5 +12,6 @@ data class Account(
         @Column(length = 784) val publicKey: String = "",
         @Transient val hash: String = ""
 ) {
+    @JsonIgnore
     fun isValid(): Boolean = hash.length == 64 && publicKey.length == 392
 }
