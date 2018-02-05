b9e2ca2abdf310849f97e314c6f3cfcdddb36382
SprinterGameDev <sprinter@ozersk.com>
2018-02-05 10:47:24 +0500

    static first client data. for example.

--- src/main/kotlin/com/bitclave/node/controllers/ClientDataController.kt
+++ src/main/kotlin/com/bitclave/node/controllers/ClientDataController.kt
@@ -1,19 +1,20 @@
 package com.bitclave.node.controllers
 
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/client/{id}/")
 class ClientDataController {
 
-    val result: Map<String, String> = hashMapOf("firstName" to "My First Name",
+    val firstClientData: Map<String, String> = hashMapOf("firstName" to "My First Name",
             "secondName" to "My Second Name")
-    val emptyResult: Map<String, String> = HashMap()
 
     @RequestMapping(method = [RequestMethod.GET])
     fun getData(@PathVariable("id") clientId: String): CompletableFuture<Map<String, String>> {
-        return CompletableFuture.completedFuture(if ("first" == clientId) result else emptyResult);
+        return CompletableFuture.completedFuture(
+                if ("first" == clientId) firstClientData else Collections.emptyMap());
     }
 
     @RequestMapping(method = [RequestMethod.PATCH])
