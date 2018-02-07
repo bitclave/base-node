447d21693335b01c75d3fb883318337e235cef02
Anton Bukov <k06aaa@gmail.com>
2018-02-07 12:27:33 +0300

    A few fixes

--- src/main/kotlin/com/bitclave/node/repository/account/EthereumAccountRepositotyImpl.kt
+++ src/main/kotlin/com/bitclave/node/repository/account/EthereumAccountRepositotyImpl.kt
@@ -1,19 +1,19 @@
 package com.bitclave.node.repository.account
 
 import com.bitclave.node.repository.models.Account
-import com.bitclave.node.solidity.AccountContract
+//import com.bitclave.node.solidity.AccountContract
 import org.springframework.stereotype.Component
 
 @Component
-class EthereumAccountRepositoryImpl(val contract: AccountContract) : AccountRepository {
+class EthereumAccountRepositoryImpl(val contract: Account) : AccountRepository {
 
-    override fun saveAccount(id: String, publicKey: String): Boolean {
-        var tx = contract.save(id, publicKey).send()
-        return true //todo: check tx status
+    override fun saveAccount(id: String, publicKey: String) {
+        //var tx = contract.save(id, publicKey).send()
     }
 
     override fun findById(id: String): Account? {
-        return contract.publicKeyXById(id) + contract.publicKeyYById(id)
+        //return contract.publicKeyXById(id) + contract.publicKeyYById(id)
+        return null
     }
 
 }
