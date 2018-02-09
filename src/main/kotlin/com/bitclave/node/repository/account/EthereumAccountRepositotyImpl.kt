d62ad6b33268875a6269517d523b8a362c168463
Anton Bukov <k06aaa@gmail.com>
2018-02-09 16:14:18 +0300

    Some work on contract

--- src/main/kotlin/com/bitclave/node/repository/account/EthereumAccountRepositotyImpl.kt
+++ src/main/kotlin/com/bitclave/node/repository/account/EthereumAccountRepositotyImpl.kt
@@ -1,19 +1,20 @@
 package com.bitclave.node.repository.account
 
+import java.math.BigInteger
+
 import com.bitclave.node.repository.models.Account
-//import com.bitclave.node.solidity.AccountContract
+import com.bitclave.node.solidity.generated.AccountContract
 import org.springframework.stereotype.Component
 
 @Component
-class EthereumAccountRepositoryImpl(val contract: Account) : AccountRepository {
+class EthereumAccountRepositoryImpl(val contract: AccountContract) : AccountRepository {
 
     override fun saveAccount(id: String, publicKey: String) {
-        //var tx = contract.save(id, publicKey).send()
+        var tx = contract.save(BigInteger(id), BigInteger(publicKey), BigInteger(publicKey)).send()
     }
 
     override fun findById(id: String): Account? {
-        //return contract.publicKeyXById(id) + contract.publicKeyYById(id)
-        return null
+        return Account("", ""+contract.publicKeyXById(BigInteger(id))+contract.publicKeyYById(BigInteger(id)))
     }
 
 }
--- src/main/resources/AccountContract.sol
+++ src/main/resources/AccountContract.sol
@@ -1,10 +1,35 @@
 pragma solidity ^0.4.0;
 
+//import './src/main/resources/StorageContract.sol';
 
-contract AccountContract {
+contract IStorageContractClient {
+
+    uint256 public storageIdentifier;
+
+}
+
+contract StorageContract {
+
+    mapping (uint256 => uint256) public dict;
+
+    function set(uint256 key, uint256 value) public {
+        uint256 id = IStorageContractClient(msg.sender).storageIdentifier();
+        dict[uint256(sha3(id, key))] = value;
+    }
+
+    function get(uint256 key) public returns(uint256) {
+        uint256 id = IStorageContractClient(msg.sender).storageIdentifier();
+        return dict[uint256(sha3(id, key))];
+    }
+
+}
+
+contract AccountContract is IStorageContractClient {
 
     mapping(uint256 => uint256) public publicKeyXById;
     mapping(uint256 => uint256) public publicKeyYById;
+
+    uint256 constant public storageIdentifier = uint256(sha3("AccountContract"));
 
     function save(uint256 id, uint256 publicKeyX, uint256 publicKeyY) public {
         publicKeyXById[id] = publicKeyX;
