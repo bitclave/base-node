276ec3280af3bda5c3fc765788dd6847f6cb24ef
Anton Bukov <k06aaa@gmail.com>
2018-02-13 18:43:30 +0300

    Improve solution

--- src/main/kotlin/com/bitclave/node/repository/account/EthereumAccountRepositotyImpl.kt
+++ src/main/kotlin/com/bitclave/node/repository/account/EthereumAccountRepositotyImpl.kt
@@ -1,7 +1,5 @@
 package com.bitclave.node.repository.account
 
-import java.math.BigInteger
-
 import com.bitclave.node.repository.models.Account
 import com.bitclave.node.solidity.generated.AccountContract
 import org.springframework.stereotype.Component
@@ -9,12 +7,12 @@ import org.springframework.stereotype.Component
 @Component
 class EthereumAccountRepositoryImpl(val contract: AccountContract) : AccountRepository {
 
-    override fun saveAccount(id: String, publicKey: String) {
-        var tx = contract.save(BigInteger(id), BigInteger(publicKey), BigInteger(publicKey)).send()
+    override fun saveAccount(publicKey: String) {
+        contract.registerPublicKey(publicKey).send()
     }
 
-    override fun findById(id: String): Account? {
-        return Account("", ""+contract.publicKeyXById(BigInteger(id))+contract.publicKeyYById(BigInteger(id)))
+    override fun findByPublicKey(key: String): Account? {
+        return Account(key)
     }
 
 }
--- src/main/resources/AccountContract.sol
+++ src/main/resources/AccountContract.sol
@@ -17,23 +17,64 @@ contract StorageContract {
         dict[uint256(sha3(id, key))] = value;
     }
 
+    function erase(uint256 key) public {
+        uint256 id = IStorageContractClient(msg.sender).storageIdentifier();
+        delete dict[uint256(sha3(id, key))];
+    }
+
     function get(uint256 key) public returns(uint256) {
         uint256 id = IStorageContractClient(msg.sender).storageIdentifier();
         return dict[uint256(sha3(id, key))];
+    }
+
+    function length(uint256 key) public returns(uint) {
+        uint256 id = IStorageContractClient(msg.sender).storageIdentifier();
+        uint length = 0;
+        while (dict[uint256(sha3(id, key + length))] != 0) {
+            length++;
+        }
+        return length;
     }
 
 }
 
 contract AccountContract is IStorageContractClient {
 
-    mapping(uint256 => uint256) public publicKeyXById;
-    mapping(uint256 => uint256) public publicKeyYById;
+    StorageContract public storageContract;
 
     uint256 constant public storageIdentifier = uint256(sha3("AccountContract"));
+    uint256 constant public publicKeyField = uint256(sha3("publicKeyField"));
+    uint256 constant public infoField = uint256(sha3("infoField"));
 
-    function save(uint256 id, uint256 publicKeyX, uint256 publicKeyY) public {
-        publicKeyXById[id] = publicKeyX;
-        publicKeyYById[id] = publicKeyY;
+    function AccountContract(StorageContract _storageContract) public {
+        storageContract = (_storageContract != address(0)) ? _storageContract : new StorageContract();
+    }
+
+    function isRegisteredPublicKey(string publicKey) public constant returns(bool) {
+        return storageContract.get(uint256(sha3(publicKeyField, publicKey))) != 0;
+    }
+
+    function registerPublicKey(string publicKey) public {
+        storageContract.set(uint256(sha3(publicKeyField, publicKey)), 1);
+    }
+
+    function infoLength(uint256 hash) public returns(uint) {
+        return storageContract.length(uint256(sha3(infoField, hash)));
+    }
+
+    function info(uint256 hash, uint index) public returns(bytes32) {
+        return bytes32(storageContract.get(uint256(sha3(infoField, hash)) + index));
+    }
+
+    function setInfo(uint256 hash, uint index, bytes32 value) public {
+        storageContract.set(uint256(sha3(infoField, hash)) + index, uint256(value));
+    }
+
+    function setInfos(uint256 hash, bytes32[] values) public {
+        for (uint i = 0; i < values.length; i++) {
+            storageContract.set(uint256(sha3(infoField, hash)) + i, uint256(values[i]));
+        }
+        storageContract.erase(uint256(sha3(infoField, hash)) + values.length);
     }
 
 }
