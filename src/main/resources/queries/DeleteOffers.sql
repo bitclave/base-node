6b25ad0a108c66acdbdfdc82d4c1bae226959fda
Mark Shwartzman <mark@bitclave.com>
2019-01-26 08:46:22 -0800

    sql examples to delete "dangling by offer" offerSearches

--- src/main/resources/queries/DeleteOffers.sql
+++ src/main/resources/queries/DeleteOffers.sql
@@ -1,4 +1,4 @@
-
+--delete  ALL offers
 delete from offer_search_events;
 delete from offer_search;
 
@@ -8,3 +8,18 @@ delete from offer_tags;
 delete from offer_rules;
 delete from offer_compare;
 delete from offer;
+--~delete  ALL offers
+
+--delete offerSearch with no matching offer
+delete from offer_search_events where offer_search_events.offer_search_id in
+	(select offer_search.id from offer_search where offer_search.id not in
+		(
+			select offer.id from offer
+		));
+
+delete from offer_search where offer_search.id not in
+	(
+		select offer.id from offer
+	);
+--~delete offerSearch with no matching offer
+
