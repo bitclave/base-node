41ad281fcf6bf642efe1abd7cd8bd90a11be416c
Mark Shwartzman <mark@bitclave.com>
2019-01-26 07:59:54 -0800

    added file with sample sql to delete all offers and offerSearches

---
+++ src/main/resources/queries/DeleteOffers.sql
@@ -0,0 +1,10 @@
+
+delete from offer_search_events;
+delete from offer_search;
+
+delete from offer_price_rules;
+delete from offer_price;
+delete from offer_tags;
+delete from offer_rules;
+delete from offer_compare;
+delete from offer;
