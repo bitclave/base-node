bc78e931149e949d1bce760570eea3a4bcce45e1
AndreyShashlov <andrey@bitclave.com>
2019-02-19 16:03:48 +0300

    update migration V2 for updatedAt/createdAt for search request model.

--- src/main/resources/db/migration/V2__updatedAt.sql
+++ src/main/resources/db/migration/V2__updatedAt.sql
@@ -7,3 +7,7 @@ alter table offer_search
 alter table offer add column created_at timestamp default now();
 
 alter table offer add column updated_at timestamp default now();
+
+alter table search_request add column created_at timestamp default now();
+
+alter table search_request add column updated_at timestamp default now();
