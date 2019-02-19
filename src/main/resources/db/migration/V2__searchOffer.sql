71810784a28fa1cde01c3d5577f6b9c92900361a
AndreyShashlov <andrey@bitclave.com>
2019-02-19 14:13:37 +0300

    update migration V2 for updatedAt/createdAt for Offer model.

--- src/main/resources/db/migration/V2__searchOffer.sql
+++
@@ -1,5 +0,0 @@
-alter table offer_search
-  alter column last_updated type timestamp using last_updated::timestamp;
-
-alter table offer_search
-  RENAME COLUMN last_updated to updated_at;
---
+++ src/main/resources/db/migration/V2__updatedAt.sql
@@ -0,0 +1,9 @@
+alter table offer_search
+  alter column last_updated type timestamp using last_updated::timestamp;
+
+alter table offer_search
+  rename column last_updated to updated_at;
+
+alter table offer add column created_at timestamp default now();
+
+alter table offer add column updated_at timestamp default now();
