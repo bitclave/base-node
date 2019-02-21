6eb5f4f26a15259f90d339612457806f83268fc1
AndreyShashlov <andrey@bitclave.com>
2019-02-21 13:47:16 +0300

    make all field as immutable!

--- src/main/kotlin/com/bitclave/node/repository/models/SignedRequest.kt
+++ src/main/kotlin/com/bitclave/node/repository/models/SignedRequest.kt
@@ -3,7 +3,7 @@ package com.bitclave.node.repository.models
 data class SignedRequest<out T>(
         val data: T? = null,
         val pk: String = "",
-        var sig: String = "",
+        val sig: String = "",
         val nonce: Long = 0,
         val rawData: String = ""
 )
