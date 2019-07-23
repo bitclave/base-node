4e688374b456ceec6d5d3a14108b6081d0b58f50
batalin <antbat2009@gmail.com>
2019-07-23 17:02:28 +0300

    codestyle fixing

--- src/test/kotlin/com/bitclave/node/search/offer/OfferSearchServiceTest.kt
+++ src/test/kotlin/com/bitclave/node/search/offer/OfferSearchServiceTest.kt
@@ -58,8 +58,6 @@ import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.test.annotation.DirtiesContext
