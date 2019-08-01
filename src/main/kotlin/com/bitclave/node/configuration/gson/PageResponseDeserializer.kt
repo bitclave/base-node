ff133c3fc228bc4e5d7681a9c139bd126c534488
AndreyShashlov <andrey@bitclave.com>
2019-08-01 16:57:26 +0300

    code improvements.

--- src/main/kotlin/com/bitclave/node/configuration/gson/PageResponseDeserializer.kt
+++ src/main/kotlin/com/bitclave/node/configuration/gson/PageResponseDeserializer.kt
@@ -15,27 +15,27 @@ open class PageResponseDeserializer : JsonDeserializer<Page<*>> {
     val pageableType = object : TypeToken<PageRequest>() {}.type
 
     override fun deserialize(
-        json: JsonElement?,
-        typeOfT: Type?,
-        context: JsonDeserializationContext?
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
     ): Page<*> {
         val parameterizedType = typeOfT as ParameterizedType
         val dataType = parameterizedType.actualTypeArguments[0]
-        val response = json!!.asJsonObject
+        val response = json.asJsonObject
 
         var pageable: PageRequest? = null
         var content: List<Any>? = null
         var total: Long = 0
 
         if (response.has("pageable") && response.get("pageable").isJsonObject) {
-            pageable = context!!.deserialize<PageRequest>(response.getAsJsonObject("pageable"), pageableType)
+            pageable = context.deserialize<PageRequest>(response.getAsJsonObject("pageable"), pageableType)
         }
 
         if (response.has("content") && response.get("content").isJsonArray) {
             val array = response.getAsJsonArray("content")
             content = arrayListOf()
             for (item in array) {
-                content.add(context!!.deserialize(item, dataType))
+                content.add(context.deserialize(item, dataType))
             }
         }
 
--- src/main/kotlin/com/bitclave/node/configuration/gson/PageWithCountersResponseDeserializer.kt
+++ src/main/kotlin/com/bitclave/node/configuration/gson/PageWithCountersResponseDeserializer.kt
@@ -2,25 +2,28 @@ package com.bitclave.node.configuration.gson
 
 import com.bitclave.node.repository.models.controllers.OffersWithCountersResponse
 import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.reflect.TypeToken
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import java.lang.reflect.Type
-import com.google.gson.Gson
 
-class PageWithCountersResponseDeserializer : PageResponseDeserializer() {
+class PageWithCountersResponseDeserializer : JsonDeserializer<OffersWithCountersResponse> {
+
     private val pageTokenType = object : TypeToken<PageImpl<Long>>() {}.type
+    private val mapTokenType = object : TypeToken<Map<String, Map<String, Int>>>() {}.type
+
     override fun deserialize(
-        json: JsonElement?,
-        typeOfT: Type?,
-        context: JsonDeserializationContext?
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
     ): OffersWithCountersResponse {
-        val one = super.deserialize(json, pageTokenType, context) as Page<Long>
-        val response = json!!.asJsonObject
-        val empMapType = object : TypeToken<Map<String, Map<String, Int>>>() {}.type
+        val page = context.deserialize<Page<Long>>(json, pageTokenType)
+        val response = json.asJsonObject
         val rawCounters = response.getAsJsonObject("counters")
-        val counters: Map<String, Map<String, Int>> = Gson().fromJson(rawCounters, empMapType)
-        return OffersWithCountersResponse(counters, one)
+        val counters = context.deserialize<Map<String, Map<String, Int>>>(rawCounters, mapTokenType)
+
+        return OffersWithCountersResponse(counters, page)
     }
 }
