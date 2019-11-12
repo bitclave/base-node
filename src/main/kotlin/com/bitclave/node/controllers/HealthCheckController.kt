a35c05f3586e7352c80f269f86feb2a76cdebb5c
AndreyShashlov <andrey@bitclave.com>
2019-11-12 13:38:51 +0300

    api for get version number. Code improvemtns.

--- src/main/kotlin/com/bitclave/node/controllers/HealthCheckController.kt
+++ src/main/kotlin/com/bitclave/node/controllers/HealthCheckController.kt
@@ -1,14 +1,17 @@
 package com.bitclave.node.controllers
 
 import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/")
 class HealthCheckController {
     // don't change this route path. it used in "k8s/staging/service-staging.yml"
     @RequestMapping(method = [RequestMethod.GET], value = ["health-check"])
-    fun healthCheck(): ResponseEntity<Void> = ResponseEntity.ok().build()
+    fun healthCheck(): CompletableFuture<ResponseEntity<Void>> =
+        CompletableFuture.completedFuture(ResponseEntity.ok().build())
 }
--- src/test/kotlin/com/bitclave/node/RunAllTests.kt
+++ src/test/kotlin/com/bitclave/node/RunAllTests.kt
@@ -6,6 +6,7 @@ import com.bitclave.node.account.AuthControllerTest
 import com.bitclave.node.clientData.ClientProfileControllerTest
 import com.bitclave.node.clientData.ClientProfileServiceHybridTest
 import com.bitclave.node.clientData.ClientProfileServiceTest
+import com.bitclave.node.dev.RootControllerTest
 import com.bitclave.node.file.FileControllerTest
 import com.bitclave.node.file.FileServiceTest
 import com.bitclave.node.offer.OfferControllerTest
@@ -45,6 +46,7 @@ import org.junit.runners.Suite
         RequestDataServiceHybridTest::class,
         RequestDataControllerTest::class,
         RequestDataServiceTest::class,
+        RootControllerTest::class,
         OfferControllerTest::class,
         OfferServiceTest::class,
         SearchRequestControllerV2Test::class,
