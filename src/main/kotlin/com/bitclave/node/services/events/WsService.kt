e426bf6572669e59fc68a8f81b4a159b01fe5e7f
AndreyShashlov <andrey@bitclave.com>
2019-12-18 12:24:51 +0300

    check exception of ws send method

--- src/main/kotlin/com/bitclave/node/services/events/WsService.kt
+++ src/main/kotlin/com/bitclave/node/services/events/WsService.kt
@@ -1,5 +1,6 @@
 package com.bitclave.node.services.events
 
+import com.bitclave.node.utils.Logger
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 import java.util.concurrent.CompletableFuture
@@ -12,6 +13,9 @@ class WsService(
     fun sendEvent(event: Event, payload: Any): CompletableFuture<Void> {
         return CompletableFuture.runAsync {
             messagingTemplate.convertAndSend(event.topic, payload)
+        }.exceptionally { e ->
+            Logger.error("WsService -> sendEvent: ${e.message}", e)
+            throw e
         }
     }
 }
